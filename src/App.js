a2e04f0208ae29b5a1f1b99a7ceae7b1230d9b02
Jeet Jani <jeetjani@dal.ca>
2024-05-15 16:32:49 -0300

    Add basic navbar and display blogs on home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./components/home";
+import Navbar from "./components/navbar";
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <Home />
     </div>
   );
 }
