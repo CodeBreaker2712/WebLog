4625cb9d805b6d8a3dbb88f74097f4760f2e881c
Jeet Jani <jeetjani@dal.ca>
2024-05-15 17:08:12 -0300

    display bloglist on the homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import Home from "./components/home";
-import Navbar from "./components/navbar";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Home />
+      <div className="content">
+        <Home />
+      </div>
     </div>
   );
 }
--- src/components/home.js
+++ src/components/home.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Bloglist from './Bloglist';
 
 const Home = () => {
 
@@ -10,14 +11,7 @@ const Home = () => {
 
     return ( 
         <div className="home">
-            {
-                blogs.map((blog) => (
-                    <div className="blog-preview">
-                        <h2> {blog.title} </h2>
-                        <p> Written by {blog.author} </p>
-                    </div>
-                ))
-            }
+            <Bloglist blogs = {blogs} title = "All Blogs!" />
         </div>
     );
 }
