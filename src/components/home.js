966c4c50388a9606260feae3866264266e0ce29e
Jeet Jani <jeetjani@dal.ca>
2024-05-16 16:25:54 -0300

    Add loading message while blog list being fetched

--- src/components/home.js
+++ src/components/home.js
@@ -5,22 +5,28 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState(null)
 
+    const [isFetchPending, setIsFetchPending] = useState(true)
+
     const handleDelete = (id) => {
         setBlogs(blogs.filter((blog) => blog.id !== id))
     }
 
     useEffect( () => {
-        fetch('http://localhost:8000/blogs')
+        setTimeout( () => {
+            fetch('http://localhost:8000/blogs')
             .then(res => {
                 return res.json()
             })
             .then(data => {
                 setBlogs(data)
+                setIsFetchPending(false)
             })
+        }, 1000)
     }, [])
 
     return ( 
         <div className="home">
+            {isFetchPending && <div>Loading...</div> }
             {blogs && <Bloglist blogs = {blogs} title = "All Blogs!" handleDelete = {handleDelete}/>}
         </div>
     );
