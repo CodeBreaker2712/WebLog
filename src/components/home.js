c4fa02bf21c87af42ce03c73209aa17cfceea9e6
Jeet Jani <jeetjani@dal.ca>
2024-05-18 10:50:42 -0300

    Implement error handling

--- src/components/home.js
+++ src/components/home.js
@@ -7,6 +7,8 @@ const Home = () => {
 
     const [isFetchPending, setIsFetchPending] = useState(true)
 
+    const [error, setError] = useState(null)
+
     const handleDelete = (id) => {
         setBlogs(blogs.filter((blog) => blog.id !== id))
     }
@@ -15,10 +17,20 @@ const Home = () => {
         setTimeout( () => {
             fetch('http://localhost:8000/blogs')
             .then(res => {
+                if(!res.ok)
+                {
+                    throw Error('Bad Request!! Resource doesn\'t exist')
+                }
                 return res.json()
             })
             .then(data => {
+                setError(null)
                 setBlogs(data)
+                setIsFetchPending(false)
+            })
+            .catch(err => {
+                setBlogs(null)
+                setError(err.message)
                 setIsFetchPending(false)
             })
         }, 1000)
@@ -27,6 +39,7 @@ const Home = () => {
     return ( 
         <div className="home">
             {isFetchPending && <div>Loading...</div> }
+            {error && <div>{error}</div> }
             {blogs && <Bloglist blogs = {blogs} title = "All Blogs!" handleDelete = {handleDelete}/>}
         </div>
     );
